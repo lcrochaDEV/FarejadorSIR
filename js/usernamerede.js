b87b1da15c5bbd7576d7b0b003dc74fee17ba397
Lucas Rocha <guitarralcs@gmail.com>
2022-11-02 00:46:31 -0400

    commit 02/11/2022 - criação de arquivos JSON

--- js/usernamerede.js
+++ js/usernamerede.js
@@ -1,35 +1,24 @@
 //CONEXÃO COM O ARQUIVO JSON
 function usernameTmp(){
-	//CRIAMDO TABELA
-	fetch('https://raw.githubusercontent.com/lcrochaDEV/FarejadorSIR/master/lista/usernameTmp.json')
-	  .then(function(response){ response.text()
-		.then(function(data){
-		  //PASSAGEM POR PARAMENTROS DA BUSCA NO ARQUIVO JSON
-			console.log(data);
-		  //PASSAGEM POR PARAMENTROS DA BUSCA NO ARQUIVO JSON  	  
-		});
-	  })
-}
-//ATIVA BUSCA POR CCTOS LISTADOS	
-atualizarBanda = setInterval(() => {usernameTmp();}, 2000);	
-
-let userNamerede = [
-    {
-        codigo: "Z089196",
-        nome: "D.RITA"
-    },
-    {
-        codigo: "Z328444",
-        nome: "ARLETE"
-    }
-];
-let bucarDadosUser = (usernameTmp) => {
-let cf = window[1][1][0][1].document.querySelectorAll(".listaCelulaFont");
-    cf.forEach(itens => {
-       userNamerede.forEach(user => {
-          if(itens.innerText === user.codigo){
-              console.log(itens.innerText = user.nome);
-          }
-       })
-    })
-}
+  //CRIAMDO TABELA
+  fetch('https://raw.githubusercontent.com/lcrochaDEV/FarejadorSIR/master/lista/usernameTmp.json')
+      .then(function(response){ response.json()
+      .then(function(data){
+        //PASSAGEM POR PARAMENTROS DA BUSCA NO ARQUIVO JSON
+        bucarDadosUser(data[0].userNamerede);
+            
+        //PASSAGEM POR PARAMENTROS DA BUSCA NO ARQUIVO JSON  	  
+      });
+      })
+  }
+  
+  let bucarDadosUser = (usernameTmp) => {
+  let cf = window[1][1][0][1].document.querySelectorAll(".listaCelulaFont");
+      cf.forEach(itens => {
+         usernameTmp.forEach(user => {
+            if(itens.innerText === user.codigo){
+                console.log(itens.innerText = user.nome);
+            }
+         })
+      })
+  }
